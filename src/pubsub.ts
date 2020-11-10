5188bac997a877b8828263712a09bf518c16f965
adedotunbashorun <adedotunolawale@gmail.com>
2020-11-10 23:52:04 +0100

    worked on bash file to start application

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -2,11 +2,12 @@ import { sendRequest } from './external';
 const subscribers: any = {};
 export const pubSub = () => {
 
-  const publish = async(topic: string, payload: any) => {
+  const publish = async(topic: string, data: any) => {
     if (!Array.isArray(subscribers[topic])) {
       return;
     }
     subscribers[topic].forEach(async (item: any) => {
+      const payload = {topic, data: { msg: data.message }};
       const resp = await sendRequest('POST', item.url, payload);
       return resp;
     });
